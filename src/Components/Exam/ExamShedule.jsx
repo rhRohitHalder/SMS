16e97279a00c16f8c9dcbcec28b62f48687d590a
Rohit731-prg <rohitsingha821380@gmail.com>
2025-02-04 13:27:59 +0530

    updated

--- src/Components/Exam/ExamShedule.jsx
+++ src/Components/Exam/ExamShedule.jsx
@@ -1,50 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function ExamShedule() {
-    const [subjects, setSubjects] = useState([]);
-        const [subjectDetails, setSubjectDetails] = useState({
-            subjectName: '',
-            subjectType: '',
-            class: '',
-            Sec: '',
-            time: '',
-            Date: '',
-        });
-    
-        const subjectTypes = ['English', 'Hindi', 'Maths', 'Science', 'Social Science'];
-        const classes = [5, 6, 7, 8, 9, 10, 11, 12];
-        const sec = ['A', 'B', 'C', 'D'];
+  const [subjects, setSubjects] = useState([]);
+  const [subjectDetails, setSubjectDetails] = useState({
+    subjectName: "",
+    subjectType: "",
+    class: "",
+    Sec: "",
+    time: "",
+    Date: "",
+  });
 
-        const timeList = [
-            "10:30-11:30 am",
-            "11:30-12:30 pm",
-            "12:30-1:30 pm",
-            "1:30-2:30 pm",
-            "2:30-3:30 pm",
-            "3:30-4:30 pm",
-          ];
-    
-        const handleSubmit = (e) => {
-            e.preventDefault(); // Prevent page refresh
-            setSubjects([...subjects, subjectDetails]);
-            setSubjectDetails({ // Reset form after submission
-                subjectName: '',
-                subjectType: '',
-                class: '',
-                subjectCode: '',
-                Date: '',
-            });
-        };
-    
-        const handleReset = () => {
-            setSubjectDetails({
-                subjectName: '',
-                subjectType: '',
-                class: '',
-                subjectCode: '',
-                Date: '',
-            });
-        };
+  const subjectTypes = [
+    "English",
+    "Hindi",
+    "Maths",
+    "Science",
+    "Social Science",
+  ];
+  const classes = [5, 6, 7, 8, 9, 10, 11, 12];
+  const sec = ["A", "B", "C", "D"];
+
+  const timeList = [
+    "10:30-11:30 am",
+    "11:30-12:30 pm",
+    "12:30-1:30 pm",
+    "1:30-2:30 pm",
+    "2:30-3:30 pm",
+    "3:30-4:30 pm",
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent page refresh
+    setSubjects([...subjects, subjectDetails]);
+    setSubjectDetails({
+      // Reset form after submission
+      subjectName: "",
+      subjectType: "",
+      class: "",
+      Sec: "",
+      time: "",
+      Date: "",
+    });
+  };
+
+  const handleReset = () => {
+    setSubjectDetails({
+      subjectName: "",
+      subjectType: "",
+      class: "",
+      Sec: "",
+      time: "",
+      Date: "",
+    });
+  };
   return (
     <div className="w-full h-auto px-10 py-10">
       <p className="text-5xl font-semibold">Examination</p>
@@ -104,7 +113,7 @@ function ExamShedule() {
             </select>
             <p className="mt-5 text-xl mb-2">Select Section</p>
             <select
-              value={subjectDetails.subjectCode}
+              value={subjectDetails.Sec}
               onChange={(e) =>
                 setSubjectDetails({
                   ...subjectDetails,
@@ -123,7 +132,7 @@ function ExamShedule() {
             </select>
             <p className="mt-5 text-xl mb-2">Select Time</p>
             <select
-              value={subjectDetails.subjectCode}
+              value={subjectDetails.time}
               onChange={(e) =>
                 setSubjectDetails({
                   ...subjectDetails,
@@ -140,6 +149,20 @@ function ExamShedule() {
                 </option>
               ))}
             </select>
+            <p className="mt-5 text-xl mb-2">Date</p>
+            <input
+              value={subjectDetails.Date}
+              onChange={(e) =>
+                setSubjectDetails({
+                  ...subjectDetails,
+                  Date: e.target.value,
+                })
+              }
+              placeholder="Enter Subject Name"
+              className="w-full px-4 py-4 rounded-lg"
+              type="date"
+              required
+            />
             <div className="w-full flex gap-10 flex-row mt-10 text-white">
               <button
                 type="submit"
@@ -164,12 +187,12 @@ function ExamShedule() {
           <div className="mt-5">
             <table className="w-full border-collapse border border-gray-500">
               <thead>
-                <tr className="bg-gray-400">
-                  <th className="px-4 py-2">ID</th>
-                  <th className="px-4 py-2">Subject Name</th>
+                <tr className="bg-gray-400 text-white">
+                  <th className="px-4 py-2">Exam Name</th>
                   <th className="px-4 py-2">Subject Type</th>
                   <th className="px-4 py-2">Class</th>
-                  <th className="px-4 py-2">Code</th>
+                  <th className="px-4 py-2">Section</th>
+                  <th className="px-4 py-2">Time</th>
                   <th className="px-4 py-2">Date</th>
                 </tr>
               </thead>
@@ -182,11 +205,11 @@ function ExamShedule() {
                         index % 2 === 0 ? "bg-gray-200" : "bg-gray-300"
                       }`}
                     >
-                      <td className=" px-4 py-2">{index + 1}</td>
                       <td className=" px-4 py-2">{subject.subjectName}</td>
                       <td className=" px-4 py-2">{subject.subjectType}</td>
                       <td className=" px-4 py-2">{subject.class}</td>
-                      <td className=" px-4 py-2">{subject.subjectCode}</td>
+                      <td className=" px-4 py-2">{subject.Sec}</td>
+                      <td className=" px-4 py-2">{subject.time}</td>
                       <td className=" px-4 py-2">{subject.Date}</td>
                     </tr>
                   ))
