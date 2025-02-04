d1e330ef105595deab8b4db4a146c9c8ea213593
Rohit731-prg <rohitsingha821380@gmail.com>
2025-02-04 12:43:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Rotune from './Components/Rotune/Rotune'
+import Login from './Components/Login/Login'
+
 
 function App() {
   return (
     <div>
+      <Login/>
       <Rotune/>
     </div>
   )
