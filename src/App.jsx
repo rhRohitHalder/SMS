56ad707591f2266d290239a59946d897c2dc1e08
Rohit731-prg <rohitsingha821380@gmail.com>
2025-02-04 18:54:51 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 
-import Sidebar from "./Components/Sidebar/Sidebar";
 import React from "react";
-import Notice from "../src/Components/Notice Board/Notice";
-import Hostel from "./Components/Hostel/Hostel";
-import Message from "./Components/Message/Message";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="flex">
-      <Sidebar />
-      <div>
-        <Message />
-      </div>
+      <Login/>
     </div>
   );
 }
