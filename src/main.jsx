3343b7e2c068ab27712b14073a97369ea57374f6
ROHIT HALDER <rohithalder4225@gmail.com>
2025-02-10 13:34:11 +0530

    updated student section

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import { Provider } from 'react-redux'
-import './index.css'
-import App from './App.jsx'
-import Login from './Components/Login/Login.jsx'
-import ExamSchedule from './Components/Exam/ExamSchedule.jsx'
-import { store } from './utils/store.js'
-import Notice from './Components/Notice Board/Notice.jsx'
-import NotFoundPage from './Components/ErrorsPages/404Page.jsx'
-import Dashboard from './Components/Dashboard/Dashboard.jsx'
-import Routine from './Components/Routine/Routine.jsx'
-import Hostel from './Components/Hostel/Hostel.jsx'
-import Message from './Components/Message/Message.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./Components/Login/Login.jsx";
+import ExamSchedule from "./Components/Exam/ExamSchedule.jsx";
+import { store } from "./utils/store.js";
+import Notice from "./Components/Notice Board/Notice.jsx";
+import NotFoundPage from "./Components/ErrorsPages/404Page.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+import Routine from "./Components/Routine/Routine.jsx";
+import Hostel from "./Components/Hostel/Hostel.jsx";
+import Message from "./Components/Message/Message.jsx";
+import StudentDetails from "./Components/student/StudentDetails.jsx";
+import StudentAdmin from "./Components/student/Student_admin_Form.jsx";
+import AllStudents from "./Components/student/FindStudent.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
@@ -23,10 +26,13 @@ createRoot(document.getElementById('root')).render(
         <Route path="/login" element={<Login />} />
         <Route path="/exam-schedule" element={<ExamSchedule />} />
         <Route path="/notice" element={<Notice />} />
-        <Route path='/routine' element={<Routine />} />
-        <Route path='/hostel' element={<Hostel />} />
-        <Route path='/message' element={<Message />} />
+        <Route path="/routine" element={<Routine />} />
+        <Route path="/hostel" element={<Hostel />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/Student-details" element={<StudentDetails />} />
+        <Route path="/student/admin" element={<StudentAdmin />} />
+        <Route path="/student/info" element={<AllStudents />} />
       </Routes>
     </BrowserRouter>
   </Provider>
-)
+);
